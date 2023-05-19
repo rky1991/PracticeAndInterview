35cb7ac7df1a0d785e29ff8b995cc78937757f1f
RahulKumar <rky1991@gmail.com>
2023-05-19 13:21:39 +0530

    All code of java learning

--- Java/Polymorphism/Derived.java
+++ Java/Polymorphism/Derived.java
@@ -1,6 +1,7 @@
 package Polymorphism;
 
 public class Derived extends Base{
+	
 	 public void Print() { 
 	        System.out.println("Derived"); 
 	    } 
--- TestNG/anottations/Anotations.java
+++ TestNG/anottations/Anotations.java
@@ -1,9 +1,59 @@
 package anottations;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Anotations {
 	
-	public static void main(String[] args) {
+	WebDriver driver;
+	
+	static {
+		
+		System.setProperty("webdriver.chrome.driver", "./jar/driver/chromedriver.exe");
 		
 	}
+	
+	@BeforeMethod
+	public void beforeTestChrome() {
+		System.out.println(" Before Method ");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+		System.out.println("Chrome Browser is launched ");
+		driver.get("https://www.google.com");
+	}
+	
+	@Test
+	public void testChrome() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("Amazon.in");
+		
+	    driver.findElement(By.xpath("(//input[@class='gNO89b' and @value='Google Search'])[2]")).click();
+	    Thread.sleep(1000);
+	    driver.findElement(By.xpath("//cite[text()='http://www.amazon.in']/../../h3")).click();
+	    
+	    WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("nav-logo-sprites")));
+		System.out.println("Homepage is found");
+		
+	 		
+	}
+	
+	@AfterMethod
+	public void CloseChrome() {
+		driver.close();
+		driver.quit();
+	}
+	
+	
+	
+	
 
 }
